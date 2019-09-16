bc37fdee500657ef56a9f203afb84041f78d5fc1
Pardip Bhatti <gpcoders.pardip@gmail.com>
2019-09-16 08:44:26 +0530

    working on many to many relations

--- public/js/app.js
+++ public/js/app.js
@@ -49507,8 +49507,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/pardipbhatti/Documents/laravel-blog/Laravel-Blog/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/pardipbhatti/Documents/laravel-blog/Laravel-Blog/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/gugu/Documents/laravel/laravel/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/gugu/Documents/laravel/laravel/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
